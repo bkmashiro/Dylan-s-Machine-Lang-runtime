fa6f6840c49da5538648902d007e79138ee0f081
bkmashiro <dylan_233@foxmail.com>
2021-11-26 14:03:00 +0800

    .

--- Dylan's Machine Lang runtime/Program.cs
+++ Dylan's Machine Lang runtime/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Dylan_s_Machine_Lang_runtime
 {
@@ -8,30 +9,73 @@ namespace Dylan_s_Machine_Lang_runtime
         static void Main(string[] args)
         {
             ShowWelcome();
-
+            string cmdPath = "";
+            if (args.Length > 0)
+            {
+                cmdPath = args[0];
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("===Guide Line===");
+            Console.WriteLine("If the address start from 00, DMLR will initialize memory form 00");
+            Console.WriteLine("Or the address doesn't start from 00, You shall input a formated text like this:");
+            Console.WriteLine("F0,20;F1,00;F2,22;F3;02;");
+            Console.WriteLine("it should be:  \nMemory_address,Contents;\n (split them by ',') ");
+            Console.WriteLine("and split the whole address-content unit by ';'");
+            Console.WriteLine("In which the enter and space will be ignored.");
+            Console.WriteLine();
+            Console.WriteLine();
+            if (cmdPath==""|| !File.Exists(cmdPath))
+            {
+                Console.WriteLine("Please Input a txt file:");
+                while (true)
+                {
+                    cmdPath = Console.ReadLine().Trim();
+                    if (File.Exists(cmdPath))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unable to access file"+cmdPath+",Please try again");
+                    }
+                }
+            }
+            string text = File.ReadAllText(cmdPath);
+            if (text.Contains(','))
+            {
+                Console.WriteLine("Loading Complex Memory format file...");
+                Console.WriteLine();
+                MachineLangAnalyser machineLangAnalyser = new MachineLangAnalyser();
+                machineLangAnalyser.LoadMem(text);
+                Console.WriteLine("Input the HEX memory address entry(where the program counter starts):");
+                machineLangAnalyser.ExecuteFrom(Console.ReadLine().Trim().HexToInt());
+            }
+            else
+            {
+                Console.WriteLine("Loading Simple Memory format file...");
+                Console.WriteLine();
+                MachineLangAnalyser machineLangAnalyser = new MachineLangAnalyser();
+                machineLangAnalyser.LoadIns(text);
+                machineLangAnalyser.ExecuteFrom(0);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
 
         static void ShowWelcome()
         {
             Console.WriteLine(@"
-                                ██████╗  ███╗   ███╗ ██╗      ██████╗ 
-                                ██╔══██╗ ████╗ ████║ ██║      ██╔══██╗
-                                ██║  ██║ ██╔████╔██║ ██║      ██████╔╝
-                                ██║  ██║ ██║╚██╔╝██║ ██║      ██╔══██╗
-                                ██████╔╝ ██║ ╚═╝ ██║ ███████╗ ██║  ██║
-                                ╚═════╝  ╚═╝     ╚═╝ ╚══════╝ ╚═╝  ╚═╝
+                                ███████╗ ███╗   ███╗ ██╗      ██████╗ 
+                                ██╔════╝ ████╗ ████║ ██║      ██╔══██╗
+                                ███████╗ ██╔████╔██║ ██║      ██████╔╝
+                                ╚════██║ ██║╚██╔╝██║ ██║      ██╔══██╗
+                                ███████║ ██║ ╚═╝ ██║ ███████╗ ██║  ██║
+                                ╚══════╝ ╚═╝     ╚═╝ ╚══════╝ ╚═╝  ╚═╝
+                                   
                                    ");
-            Console.WriteLine("                                Dylan's machine language runtime version 1.0.0");
-            MachineLangAnalyser machineLangAnalyser = new MachineLangAnalyser();
-            machineLangAnalyser.LoadIns(
-                @"
-2101
-2202
-5312
-3303
-D003
-C000");
-            machineLangAnalyser.ExecuteFrom(0);
+            Console.WriteLine("                          Shi Yuzhe's machine language runtime version 1.0.0");
+            
         }
     }
 
@@ -67,35 +111,61 @@ C000");
         static int programCounter;
         static bool isHalted = false;
         static int MAX_STEP = 500;
+        Queue<(int, string)> UserCMD = new Queue<(int, string)>();
         public void Satrt(int counter = 0)
         {
+            programCounter = counter;
+            Logger.pc = programCounter;
+
             isHalted = false;
             int step = 0;
-            while (step++<MAX_STEP)
+            while (step++ < MAX_STEP)
             {
+                //perform usercommand
+                (int, string) userCmd;
+                if (UserCMD.TryPeek(out userCmd))
+                {
+                    if (userCmd.Item1<=programCounter)
+                    {
+                        while (UserCMD.TryDequeue(out userCmd))
+                        {
+                            Execute(userCmd.Item2);
+                        }
+                    }
+                }
                 if (isHalted) return;
-                Execute(Memory.MemGet(programCounter).IntToIns());
-                ++programCounter;
+                Execute(Memory.MemGet(programCounter++).IntTo2Hex() + Memory.MemGet(programCounter++).IntTo2Hex());
+                Logger.pc = programCounter;
             }
         }
 
         public void InitIns(string ins)
         {
             int addr = 0;
-            foreach (var instructon in ins.Split('\r'))
+            foreach (var instruction in ins.Split('\r'))
             {
-                if (instructon.Trim() == "") continue;
-                Instructions.Enqueue(instructon.Trim());
-                Memory.MemSet(addr++,instructon.HexToInt());
+                string t = instruction.Trim();
+                if (t[0] == '#' || t == "") continue;
+                if (t[0]=='$')
+                {
+                    //is an command
+                    UserCMD.Enqueue((addr, t[1..]));
+                    continue;
+                }
+                string s = instruction.Trim().Substring(0, 4);
+                Instructions.Enqueue(s);
+                Memory.MemSet(addr++, s.Substring(0, 2).HexToInt());
+                Memory.MemSet(addr++, s.Substring(2, 2).HexToInt());
             }
             log($"Loaded instructions form MEM_0 to MEM_{addr}", LogLevel.info);
             programCounter = 0;
         }
-        public void InitMem(string initCmd) 
+        public void InitMem(string initCmd)
         {
             string[] mems = initCmd.Split(';');
             foreach (var mem in mems)
             {
+                if (mem.Trim() == "") continue;
                 string[] ss = mem.Split(',');
                 int addr = ss[0].HexToInt();
                 int val = ss[1].HexToInt();
@@ -103,69 +173,27 @@ C000");
             }
         }
         #region log
-        static void log(string x, LogLevel level)
+        static void log(string x,LogLevel level)
         {
-            Console.WriteLine($"[{levelText[(int)level],10}]{x}");
+            Logger.log(x, level);
         }
-        static string[] levelText = { "fatal", "warning", "info" };
-        enum LogLevel
-        {
-            fatal,
-            warning,
-            info,
-        }
-
         #endregion
+
+        static IOperation[] operations = { new Op1(), new Op2(), new Op3(), new Op4(), new Op5(), new Op6(), new Op7(), new Op8(), new Op9(), new OpA(), new OpB(), new OpC(), new OpD(), new OpE(), new OpF(), new __UNKNOWN() };
         private void Execute(string Instruction)
         {
             if (isHalted) return;
-            char insType = Instruction[0];
-            switch (insType)
+            int insType = Instruction[0].ToString().HexToInt();
+            if (insType > 0 && insType < 16)
             {
-                case '1':
-                    RunIns(Instruction, new Op1());
-                    break;
-                case '2':
-                    RunIns(Instruction, new Op2());
-                    break;
-                case '3':
-                    RunIns(Instruction, new Op3());
-                    break;
-                case '4':
-                    RunIns(Instruction, new Op4());
-                    break;
-                case '5':
-                    RunIns(Instruction, new Op5());
-                    break;
-                case '6':
-                    RunIns(Instruction, new Op6());
-                    break;
-                case '7':
-                    RunIns(Instruction, new Op7());
-                    break;
-                case '8':
-                    RunIns(Instruction, new Op8());
-                    break;
-                case '9':
-                    RunIns(Instruction, new Op9());
-                    break;
-                case 'A':
-                    RunIns(Instruction, new OpA());
-                    break;
-                case 'B':
-                    RunIns(Instruction, new OpB());
-                    break;
-                case 'C':
-                    RunIns(Instruction, new OpC());
-                    break;
-                case 'D':
-                    RunIns(Instruction, new OpD());
-                    break;
-                default:
-                    break;
+                RunIns(Instruction, operations[insType - 1]);
+            }
+            else
+            {
+                RunIns(Instruction, new __UNKNOWN());
             }
         }
-        private void RunIns(string ins,IOperation operation)
+        private void RunIns(string ins, IOperation operation)
         {
             operation.execute(ins);
         }
@@ -230,7 +258,7 @@ C000");
                 int regAddressR = instruction.Substring(1, 1).HexToInt();
                 int regAddressS = instruction.Substring(2, 1).HexToInt();
                 int regAddressT = instruction.Substring(3, 1).HexToInt();
-                Register.RegisterSet(regAddressR,Register.RegisterGet(regAddressS)+Register.RegisterGet(regAddressT));
+                Register.RegisterSet(regAddressR, Register.RegisterGet(regAddressS) + Register.RegisterGet(regAddressT));
                 log($"ADD REG_{instruction.Substring(2, 1)} REG_{instruction.Substring(3, 1)},stored in REG_{instruction.Substring(1, 1)}", LogLevel.info);
                 return true;
             }
@@ -301,7 +329,7 @@ C000");
                 return true;
             }
 
-            int move_right(int val,int n)
+            int move_right(int val, int n)
             {
                 int N = 32;
                 return (val >> (N - n) | (val << n));
@@ -314,7 +342,7 @@ C000");
             {
                 int regAddressR = instruction.Substring(1, 2).HexToInt();
                 int memAddressXY = instruction.Substring(2, 2).HexToInt();
-                if (Register.RegisterGet(0)==Register.RegisterGet(regAddressR))
+                if (Register.RegisterGet(0) == Register.RegisterGet(regAddressR))
                 {
                     //Jump
                     programCounter = memAddressXY;
@@ -340,10 +368,67 @@ C000");
         }
         class OpD : IOperation
         {
-            public string Format { get; set; } = "D 0 XY";
+            public string Format { get; set; } = "D _ XY";
             public bool execute(string instruction)
             {
-                log($"MEM_{instruction.Substring(2,2)} = {Memory.MemGet(instruction.Substring(2,2).HexToInt())}", LogLevel.fatal);
+                switch (instruction[1])
+                {
+                    case '0':
+                        log($"MEM_{instruction.Substring(2, 2)} = {Memory.MemGet(instruction.Substring(2, 2).HexToInt())}", LogLevel.fatal);
+                        break;
+                    case '1':
+                        log($"REG_{instruction.Substring(3, 1)} = {Register.RegisterGet(instruction.Substring(3, 1).HexToInt())}", LogLevel.fatal);
+                        break;
+                    case '2':
+                        Memory.PrintAll();
+                        break;
+                    case '3':
+                        Register.PrintAll();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+
+
+        }
+        class OpE : IOperation
+        {
+            public string Format { get; set; } = "E";
+            public bool execute(string instruction)
+            {
+                return true;
+            }
+        }
+        class OpF : IOperation
+        {
+            public string Format { get; set; } = "F";
+            public bool execute(string instruction)
+            {
+                return true;
+            }
+        }
+        class __UNKNOWN : IOperation
+        {
+            public string Format { get; set; } = "0000";
+            public bool execute(string instruction)
+            {
+                log($"UNKNOWN OPERATION!stack trace back:\n{instruction}@#{programCounter-2:X}", LogLevel.fatal);
+                Console.WriteLine("If you insist to continue, enter Y(y),or the programe will be enforced HALT by SMLR");
+                string s = Console.ReadKey().KeyChar.ToString();
+                if (s.ToUpper() == "Y")
+                {
+                    Console.WriteLine();
+                    log($"Manually jumpped unsolved instruction @MEM_{programCounter-2:X2}", LogLevel.warning);
+                }
+                else
+                {
+                    isHalted = true;
+                    Console.WriteLine();
+                    log($"Automatically HALT at MEM_{programCounter-2:X2}", LogLevel.warning);
+                    return true;
+                }
                 return true;
             }
         }
@@ -359,16 +444,9 @@ C000");
         }
         static List<Register> _registers = new List<Register>();
         static Dictionary<int, Register> registers = new Dictionary<int, Register>();
-        void log(string x, LogLevel level)
+        static void log(string x, LogLevel level)
         {
-            Console.WriteLine($"[{levelText[(int)level],10}]{x}");
-        }
-        string[] levelText = { "fatal", "warning", "info" };
-        enum LogLevel
-        {
-            fatal,
-            warning,
-            info,
+            Logger.log(x, level);
         }
         public void RegisterSet(int address, int value)
         {
@@ -408,6 +486,17 @@ C000");
                 }
             }
         }
+        public void PrintAll()
+        {
+            log($"===START REGISTER SNAPSHOT===", LogLevel.alert);
+
+            foreach (var item in _registers)
+            {
+                log($"   REG_{item.Address.IntTo2Hex()} = {item.Value.IntTo2Hex()}", LogLevel.alert);
+            }
+            log($"===END REGISTER SNAPSHOT===", LogLevel.alert);
+
+        }
     }
     class MLMEM
     {
@@ -420,16 +509,9 @@ C000");
         }
         private List<MemCell> _mem = new List<MemCell>();
         private Dictionary<int, MemCell> mem = new Dictionary<int, MemCell>();
-        string[] levelText = { "fatal", "warning", "info" };
-        enum LogLevel
+        static void log(string x, LogLevel level)
         {
-            fatal,
-            warning,
-            info,
-        }
-        void log(string x, LogLevel level)
-        {
-            Console.WriteLine($"[{levelText[(int)level],10}]{x}");
+            Logger.log(x, level);
         }
         public void MemSet(int address, int value)
         {
@@ -464,15 +546,22 @@ C000");
                 }
                 else
                 {
-                    log($"Try access uninitalized memory @{address}", LogLevel.warning);
+                    log($"Try access uninitalized memory @{address:X}", LogLevel.warning);
                     return 0;
                 }
             }
         }
 
-
+        public void PrintAll()
+        {
+            log("===START MEMORY SNAPSHOT===", LogLevel.alert);
+            foreach (var item in _mem)
+            {
+                log($"   MEM_{item.Address.IntTo2Hex()} = {item.Value.IntTo2Hex()}", LogLevel.alert);
+            }
+            log($"===END MEMORY SNAPSHOT===", LogLevel.alert);
+        }
     }
-
     public static class Helper
     {
         public static int HexToInt(this string s)
@@ -484,6 +573,28 @@ C000");
         public static string IntToIns(this int s)
         {
             return s.ToString("X4");
+        }
+
+        public static string IntTo2Hex(this int s)
+        {
+            return s.ToString("X2");
+        }
+    }
+    public enum LogLevel
+    {
+        fatal,
+        warning,
+        info,
+        alert,
+    }
+    public static class Logger
+    {
+        public static int pc = 0;
+        static string[] levelText = { "fatal", "warning", "info", "alert" };
+
+        public static void log(string x, LogLevel level)
+        {
+            Console.WriteLine($"#{pc,-3:X}[{levelText[(int)level]+ ']',-10}  {x}");
         }
     }
 }
