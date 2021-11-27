ec9ecb2b0d8e39c32544659dc66432d7293d3b5a
bkmashiro <dylan_233@foxmail.com>
2021-11-27 17:04:44 +0800

    fix or and xor.

--- Dylan's Machine Lang runtime/Program.cs
+++ Dylan's Machine Lang runtime/Program.cs
@@ -28,7 +28,7 @@ namespace Dylan_s_Machine_Lang_runtime
             Console.WriteLine("Every line a a character instruction.");
             Console.WriteLine();
             Console.WriteLine();
-            if (cmdPath==""|| !File.Exists(cmdPath))
+            if (cmdPath == "" || !File.Exists(cmdPath))
             {
                 Console.WriteLine("Please Input a txt file:");
                 while (true)
@@ -40,7 +40,7 @@ namespace Dylan_s_Machine_Lang_runtime
                     }
                     else
                     {
-                        Console.WriteLine("Unable to access file"+cmdPath+",Please try again");
+                        Console.WriteLine("Unable to access file" + cmdPath + ",Please try again");
                     }
                 }
             }
@@ -78,7 +78,7 @@ namespace Dylan_s_Machine_Lang_runtime
                                    
                                    ");
             Console.WriteLine("                          Shi Yuzhe's machine language runtime version 1.0.0");
-            
+
         }
     }
 
@@ -128,7 +128,7 @@ namespace Dylan_s_Machine_Lang_runtime
                 (int, string) userCmd;
                 if (UserCMD.TryPeek(out userCmd))
                 {
-                    if (userCmd.Item1<=programCounter)
+                    if (userCmd.Item1 <= programCounter)
                     {
                         while (UserCMD.TryDequeue(out userCmd))
                         {
@@ -143,7 +143,7 @@ namespace Dylan_s_Machine_Lang_runtime
                 Logger.pc = programCounter;
                 if (isHalted) return;
             }
-            log("ERROR: TOO MANY EXECUTIONS!",LogLevel.fatal);
+            log("ERROR: TOO MANY EXECUTIONS!", LogLevel.fatal);
         }
 
         public void InitIns(string ins)
@@ -153,7 +153,7 @@ namespace Dylan_s_Machine_Lang_runtime
             {
                 string t = instruction.Trim();
                 if (t[0] == '#' || t == "") continue;
-                if (t[0]=='$')
+                if (t[0] == '$')
                 {
                     //is an command
                     UserCMD.Enqueue((addr, t[1..]));
@@ -164,23 +164,33 @@ namespace Dylan_s_Machine_Lang_runtime
                 Memory.MemSet(addr++, s.Substring(0, 2).HexToInt());
                 Memory.MemSet(addr++, s.Substring(2, 2).HexToInt());
             }
-            log($"Loaded instructions form MEM_0 to MEM_{addr}", LogLevel.info);
+            log($"Loaded instructions form MEM_00 to MEM_{addr.IntTo2Hex()}", LogLevel.info);
             programCounter = 0;
         }
         public void InitMem(string initCmd)
         {
-            string[] mems = initCmd.Split(';');
+            string[] mems = initCmd.Split('\n');
+            int _tmp_addr = 0;
             foreach (var mem in mems)
             {
-                if (mem.Trim() == "") continue;
-                string[] ss = mem.Split(',');
+                string s = mem.Trim();
+                if (s == "" || s[0] == '#') continue;
+                if (s[0] == '$')
+                {
+                    UserCMD.Enqueue((_tmp_addr, s[1..4]));
+                    continue;
+                }
+                string[] ss = s.Split(',');
                 int addr = ss[0].HexToInt();
+                _tmp_addr = addr;
                 int val = ss[1].HexToInt();
                 Memory.MemNew(addr, val);
             }
+            log($"Loaded customized instructions till {_tmp_addr.IntTo2Hex()}", LogLevel.info);
+
         }
         #region log
-        static void log(string x,LogLevel level)
+        static void log(string x, LogLevel level)
         {
             Logger.log(x, level);
         }
@@ -257,6 +267,9 @@ namespace Dylan_s_Machine_Lang_runtime
                 return true;
             }
         }
+        /// <summary>
+        /// ADD in 2's completement
+        /// </summary>
         class Op5 : IOperation
         {
             public string Format { get; set; } = "5 R S T";
@@ -265,8 +278,18 @@ namespace Dylan_s_Machine_Lang_runtime
                 int regAddressR = instruction.Substring(1, 1).HexToInt();
                 int regAddressS = instruction.Substring(2, 1).HexToInt();
                 int regAddressT = instruction.Substring(3, 1).HexToInt();
-                Register.RegisterSet(regAddressR, Register.RegisterGet(regAddressS) + Register.RegisterGet(regAddressT));
-                log($"ADD REG_{instruction.Substring(2, 1)} REG_{instruction.Substring(3, 1)},stored in REG_{instruction.Substring(1, 1)}", LogLevel.info);
+                int ans = Register.RegisterGet(regAddressS) + Register.RegisterGet(regAddressT);
+                if (ans >= (1 << 8))
+                {
+                    Register.RegisterSet(regAddressR, ans & 0b1_0000_0000);
+                    log($"ADD: REG_{instruction.Substring(1, 1)}=REG_{instruction.Substring(2, 1)}+REG_{instruction.Substring(3, 1)}={ans & 0b1_0000_0000:x}(Overflowed)", LogLevel.info);
+                }
+                else
+                {
+                    Register.RegisterSet(regAddressR, ans);
+                    log($"ADD REG_{instruction.Substring(2, 1)} REG_{instruction.Substring(3, 1)},stored in REG_{instruction.Substring(1, 1)}", LogLevel.info);
+                }
+
                 return true;
             }
         }
@@ -275,10 +298,109 @@ namespace Dylan_s_Machine_Lang_runtime
             public string Format { get; set; } = "6 R S T";
             public bool execute(string instruction)
             {
-                //TODO
-                log($"ADD(float) REG_{instruction.Substring(1, 1)} REG_{instruction.Substring(2, 1)},stored in REG_{instruction.Substring(3, 1)}", LogLevel.info);
-
+                int addr_S = instruction.Substring(2, 1).HexToInt();
+                int addr_T = instruction.Substring(3, 1).HexToInt();
+                int addr_R = instruction.Substring(1, 1).HexToInt();
+                log($"ADD(float) REG_{instruction.Substring(2, 1)} REG_{instruction.Substring(3, 1)},stored in REG_{instruction.Substring(1, 1)}", LogLevel.info);
+                double rS = Int2float(Register.RegisterGet(addr_S));
+                double rT = Int2float(Register.RegisterGet(addr_T));
+                double ans = rS + rT;
+                unsafe
+                {
+                    int offset = (1 << 10) - 1;
+                    long value = GetMemoryValue(ans);
+                    int sign = (int)(((ulong)value & 0x8000000000000000) >> 63);
+                    int e = (int)((value & 0x7FF0000000000000) >> 52) - offset;
+                    int e2 = (1 << 2) + e+1;
+                    long d = (value & 0xFFFFFFFFFFFFF);
+                    ulong t=(GetBinay(GetBaseNumber(d)));
+                    int firstOneInt = 3;
+                    for (int i = 64 - 1; i >= 0; i--)
+                    {
+                        if ((t&(0b1UL<<i))>0)
+                        {
+                            firstOneInt =i;
+                            break;
+                        }
+                    }
+                    if (firstOneInt>3)
+                    {
+                        t >>= firstOneInt - 3;
+                    }
+                    else if (firstOneInt<3)
+                    {
+                        t <<= 3 - firstOneInt;
+                    }
+                    int myEigttBitFloat = 0;
+                    if (firstOneInt>3)
+                    {
+                        log($"float operation truncation error", LogLevel.warning);
+                    }
+                    else
+                    {
+                        //Nothing
+                    }
+                    myEigttBitFloat = (sign << 7) | (e2 << 4) | ((int)t);
+                    //Console.WriteLine(myEigttBitFloat);
+                    Register.RegisterSet(addr_R, myEigttBitFloat);
+                }
                 return true;
+            }
+
+            private double Int2float(int i)
+            {
+                int sign = (i & 0b1000_0000) == 0 ? 1 : -1;
+                int exceed = ((i & 0b0111_0000) >> 4) - 0b0100;
+                double t = 0;
+                int digits = i & 0b0000_1111; double weight = 1d / 16d;
+                for (int j = 0; j < 4; j++)
+                {
+                    t += ((digits & (1 << j)) > 0 ? 1 : 0) * weight;
+                    weight *= 2;
+                }
+                return sign * t * Math.Pow(2, exceed);
+            }
+
+            static unsafe long GetMemoryValue(double value)
+            {
+                return (*((long*)(&value)));
+            }
+            static double GetBaseNumber(long value)
+            {
+                double result = 0.0, u = 0x10000000000000;
+                long d = 0x1;
+                for (int i = 0; i < 52; i++)
+                {
+                    result += ((value & d) >> i) / u;
+                    d <<= 1;
+                    u /= 2;
+                }
+                return 1 + result;
+            }
+
+            static ulong GetBinay(double d)
+            {
+                double b = 1;
+                int counter = 63;
+                ulong ans = 0;
+                while (d>0&&b>1d/(1L<<31))
+                {
+                    if (d>=b)
+                    {
+                        ans |= 1uL << counter;
+                        d -= b;
+                        //Console.WriteLine("+"+b);
+                    }
+                    b /= 2f;
+                    --counter;
+                }
+                int cnt=0;
+                while ((ans&0b1)==0&&cnt<64)
+                {
+                    ans >>= 1;
+                    ++cnt;
+                }
+                return ans;
             }
         }
         class Op7 : IOperation
@@ -286,11 +408,11 @@ namespace Dylan_s_Machine_Lang_runtime
             public string Format { get; set; } = "7 R S T";
             public bool execute(string instruction)
             {
-                int regAddressS = instruction.Substring(1, 2).HexToInt();
-                int regAddressT = instruction.Substring(2, 1).HexToInt();
-                int regAddressR = instruction.Substring(3, 1).HexToInt();
+                int regAddressS = instruction.Substring(2, 1).HexToInt();
+                int regAddressT = instruction.Substring(3, 1).HexToInt();
+                int regAddressR = instruction.Substring(1, 1).HexToInt();
                 Register.RegisterSet(regAddressR, Register.RegisterGet(regAddressS) | Register.RegisterGet(regAddressT));
-                log($"OR REG_{instruction.Substring(1, 1)} and REG_{instruction.Substring(2, 1)},stored in REG_{instruction.Substring(3, 1)}", LogLevel.info);
+                log($"OR REG_{instruction.Substring(2, 1)} and REG_{instruction.Substring(3, 1)},stored in REG_{instruction.Substring(1, 1)}", LogLevel.info);
 
                 return true;
             }
@@ -300,11 +422,11 @@ namespace Dylan_s_Machine_Lang_runtime
             public string Format { get; set; } = "8 R S T";
             public bool execute(string instruction)
             {
-                int regAddressS = instruction.Substring(1, 2).HexToInt();
-                int regAddressT = instruction.Substring(2, 1).HexToInt();
-                int regAddressR = instruction.Substring(3, 1).HexToInt();
+                int regAddressS = instruction.Substring(2, 1).HexToInt();
+                int regAddressT = instruction.Substring(3, 1).HexToInt();
+                int regAddressR = instruction.Substring(1, 1).HexToInt();
                 Register.RegisterSet(regAddressR, Register.RegisterGet(regAddressS) & Register.RegisterGet(regAddressT));
-                log($"AND REG_{instruction.Substring(1, 1)} and REG_{instruction.Substring(2, 1)},stored in REG_{instruction.Substring(3, 1)}", LogLevel.info);
+                log($"AND REG_{instruction.Substring(2, 1)} and REG_{instruction.Substring(3, 1)},stored in REG_{instruction.Substring(1, 1)}", LogLevel.info);
 
                 return true;
             }
@@ -314,11 +436,11 @@ namespace Dylan_s_Machine_Lang_runtime
             public string Format { get; set; } = "9 R S T";
             public bool execute(string instruction)
             {
-                int regAddressS = instruction.Substring(1, 2).HexToInt();
-                int regAddressT = instruction.Substring(2, 1).HexToInt();
-                int regAddressR = instruction.Substring(3, 1).HexToInt();
+                int regAddressS = instruction.Substring(2, 1).HexToInt();
+                int regAddressT = instruction.Substring(3, 1).HexToInt();
+                int regAddressR = instruction.Substring(1, 1).HexToInt();
                 Register.RegisterSet(regAddressR, Register.RegisterGet(regAddressS) ^ Register.RegisterGet(regAddressT));
-                log($"EXCLUSIVE REG_{instruction.Substring(1, 1)} and REG_{instruction.Substring(2, 1)},stored in REG_{instruction.Substring(3, 1)}", LogLevel.info);
+                log($"EXCLUSIVE REG_{instruction.Substring(2, 1)} and REG_{instruction.Substring(3, 1)},stored in REG_{instruction.Substring(1, 1)}", LogLevel.info);
 
                 return true;
             }
@@ -381,7 +503,7 @@ namespace Dylan_s_Machine_Lang_runtime
                 switch (instruction[1])
                 {
                     case '0':
-                        log($"MEM_{instruction.Substring(2, 2)} = {Memory.MemGet(instruction.Substring(2, 2).HexToInt()):X}", LogLevel.alert);
+                        log($"MEM_{instruction.Substring(2, 2)} = {Memory.MemGet(instruction.Substring(2, 2).HexToInt()):x}", LogLevel.alert);
                         break;
                     case '1':
                         log($"REG_{instruction.Substring(3, 1)} = {Register.RegisterGet(instruction.Substring(3, 1).HexToInt()):x}", LogLevel.alert);
@@ -424,19 +546,19 @@ namespace Dylan_s_Machine_Lang_runtime
             public string Format { get; set; } = "0000";
             public bool execute(string instruction)
             {
-                log($"UNKNOWN OPERATION!stack trace back:\n{instruction}@#{programCounter-2:X}", LogLevel.fatal);
+                log($"UNKNOWN OPERATION!stack trace back:\n{instruction}@#{programCounter - 2:X}", LogLevel.fatal);
                 Console.WriteLine("If you insist to continue, enter Y(y),or the programe will be enforced HALT by SMLR");
                 string s = Console.ReadKey().KeyChar.ToString();
                 if (s.ToUpper() == "Y")
                 {
                     Console.WriteLine();
-                    log($"Manually jumpped unsolved instruction @MEM_{programCounter-2:X2}", LogLevel.warning);
+                    log($"Manually jumpped unsolved instruction @MEM_{programCounter - 2:X2}", LogLevel.warning);
                 }
                 else
                 {
                     isHalted = true;
                     Console.WriteLine();
-                    log($"Automatically HALT at MEM_{programCounter-2:X2}", LogLevel.warning);
+                    log($"Automatically HALT at MEM_{programCounter - 2:X2}", LogLevel.warning);
                     return true;
                 }
                 return true;
@@ -604,7 +726,7 @@ namespace Dylan_s_Machine_Lang_runtime
 
         public static void log(string x, LogLevel level)
         {
-            Console.WriteLine($"#{pc,-3:X}[{levelText[(int)level]+ ']',-10}  {x}");
+            Console.WriteLine($"#{pc,-3:X}[{levelText[(int)level] + ']',-10}  {x}");
         }
     }
 }
